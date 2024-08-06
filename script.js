8493cb278ec5c6cfbb446c4367ee6b129d88d935
taffou <ngasseutaffou@yahoo.co.uk>
2024-08-06 18:26:19 +0100

    Improved the design and feel

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ async function checkweather(city){
     } else{
         var data = await response.json();
 
-    // console.log(data);
-
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
